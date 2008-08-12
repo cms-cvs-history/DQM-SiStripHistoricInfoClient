c2f2f8255e4ccb57cfffcbe7faa501e595d9d05b
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-08-12 18:50:42 +0000

    added new me

--- test/TrendsWithIOVIterator/testHistoricInspector.cc
+++ test/TrendsWithIOVIterator/testHistoricInspector.cc
@@ -8,10 +8,15 @@ HistoricInspector A;
 //A.setDB("oracle://devdb10/CMS_COND_STRIP", "SiStripSummary_test2","cms_cond_strip","w3807dev","COND/Services/TBufferBlobStreamingService");
 A.setDB("sqlite_file:historicDQM.db", "SiStripSummary_test1","cms_cond_strip","w3807dev","");
 
-//A.createTrend(1, "Summary_NumberOfClusters_OffTrack@mean");
+
+//TIB
 A.createTrend(1, "Summary_NumberOfClusters_OnTrack@mean,Summary_NumberOfClusters_OffTrack@mean");
-A.createTrend(2, "Summary_NumberOfClusters_OnTrack@mean,Summary_NumberOfClusters_OffTrack@mean");
+
+//TOB
+A.createTrend(2, "Summary_NumberOfClusters_OnTrack@mean,Summary_NumberOfClusters_OffTrack@mean,Summary_cStoNCorr_OnTrack@mean,Summary_cStoNCorr_OnTrack@landauPeak");
 //A.createTrend(1, "Summary_NumberOfClusters_OffTrack@mean",51093);
-//A.createTrend(0, "NumberOfTracks@mean,NumberOfTracks@entries");
-A.createTrend(0, "NumberOfTracks_CKFTk@mean,NumberOfTracks_CKFTk@entries");
+
+
+//TK
+A.createTrend(0, "NumberOfTracks_CKFTk@mean,NumberOfTracks_CKFTk@entries,NumberOfRecHitsPerTrack_CKFTk@mean,Chi2_CKFTk@mean");
 }
