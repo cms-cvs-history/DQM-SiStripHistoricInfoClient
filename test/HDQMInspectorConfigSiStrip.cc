03385c5b1b32bfc455419678d5313e4eea049cfc
Dean Andrew Hidas <sha1-6c92d0e9fd6974661f32e167cae070c19b465ec2@cern.ch>
2009-07-30 12:47:55 +0000

    getting plotting for strips going... naming on plot labels should work now

--- test/HDQMInspectorConfigSiStrip.cc
+++ test/HDQMInspectorConfigSiStrip.cc
@@ -1,5 +1,15 @@
 #include "DQM/SiStripHistoricInfoClient/test/HDQMInspectorConfigSiStrip.h"
 
+#include "DQM/SiStripCommon/interface/SiStripFolderOrganizer.h"
+#include "DataFormats/SiStripDetId/interface/SiStripDetId.h"
+#include "DataFormats/SiStripDetId/interface/TIBDetId.h"
+#include "DataFormats/SiStripDetId/interface/TIDDetId.h"
+#include "DataFormats/SiStripDetId/interface/TOBDetId.h"
+#include "DataFormats/SiStripDetId/interface/TECDetId.h"
+
+
+#include <sstream>
+
 HDQMInspectorConfigSiStrip::HDQMInspectorConfigSiStrip ()
 {
 }
@@ -12,48 +22,27 @@ HDQMInspectorConfigSiStrip::~HDQMInspectorConfigSiStrip ()
 
 std::string HDQMInspectorConfigSiStrip::translateDetId(const uint32_t id) const
 {
-  switch (id) {
-    case 1:   
-       return "TIB";
-    case 2:   
-       return "TOB";
-    case 3:   
-       return "TID";
-    case 4:   
-       return "TEC";
-    case 11:  
-       return "TIB_Layer1";
-    case 12:  
-       return "TIB_Layer2";
-    case 13:  
-       return "TIB_Layer3";
-    case 14:  
-       return "TIB_Layer4";
-    case 21:  
-       return "TOB_Layer1";
-    case 22:  
-       return "TOB_Layer2";
-    case 23:  
-       return "TOB_Layer3";
-    case 24:  
-       return "TOB_Layer4";
-    case 25:  
-       return "TOB_Layer5";
-    case 26:  
-       return "TOB_Layer6";
-    case 311: 
-       return "TID_Side1_Layer1";
-    case 312: 
-       return "TID_Side1_Layer2";
-    case 313: 
-       return "TID_Side1_Layer3";
-    case 321: 
-       return "TID_Side2_Layer1";
-    case 322: 
-       return "TID_Side2_Layer2";
-    case 323: 
-       return "TID_Side2_Layer3";
-    default:
-      return "???";
-  };
+  std::stringstream Name;
+
+  uint32_t rawdetid = id;
+  SiStripDetId stripdet = SiStripDetId(rawdetid);
+
+  if(stripdet.subDetector() == SiStripDetId::TIB) {
+    TIBDetId tib1 = TIBDetId(rawdetid);
+    Name << "TIB";
+  } else if(stripdet.subDetector() == SiStripDetId::TID) {
+    TIDDetId tid1 = TIDDetId(rawdetid);
+    Name << "TID";
+  } else if(stripdet.subDetector() == SiStripDetId::TOB) {
+    TOBDetId tob1 = TOBDetId(rawdetid);
+    Name << "TOB";
+  } else if( stripdet.subDetector() == SiStripDetId::TEC) {
+    TECDetId tec1 = TECDetId(rawdetid);
+    Name << "TEC";
+  } else{
+    Name << "???";
+  }
+
+  return Name.str();
+
 }
